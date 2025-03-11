410931bdabf4db25aa699e8cc9223a597af6dadc
Ihor Indyk <ihor.indyk@gmail.com>
2025-03-11 15:26:47 -0700

    Update build_whl.sh

--- oss/build_whl.sh
+++ oss/build_whl.sh
@@ -41,7 +41,7 @@ function main() {
 
   export USE_BAZEL_VERSION="${BAZEL_VERSION}"
   bazel clean
-  bazel run //:requirements.update
+  #bazel run //:requirements.update
   bazel build ... --action_env MACOSX_DEPLOYMENT_TARGET='11.0'
   bazel test --verbose_failures --test_output=errors ...
 
